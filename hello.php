a08e4ccb5978fccc0d72767154578a4d546d722c
FIXLIP04 <fixlip04@gmail.com>
2024-01-26 10:58:30 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP Test </title>
+</head>
+<body>
+
+    <?php
+    // comment
+    /*
+    comment
+    */
+    $variable = "|variable|";
+    echo "Hello World!, this is my first php script " .$variable;
+    phpinfo(); 
+    ?>
+
+</body>
+</html>
+
