bdf02aa33150fb75ebe12d35cbe0e11a5ab30a39
FIXLIP04 <fixlip04@gmail.com>
2024-01-29 13:00:18 +0200

    exercise2

---
+++ Filip_tasks/ex2.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; 
+$title = "Exercise 2: "?>
+<h1> Shoes </h1>
+<p>
+A shoe is an item of footwear intended to protect and comfort the human foot. Though the human foot can adapt to varied terrains and climate conditions, it is vulnerable, and shoes provide protection. Form was originally tied to function, but over time, shoes also became fashion items. Some shoes are worn as safety equipment, such as steel-toe boots, which are required footwear at industrial worksites.
+</p>
+<?php include 'footer.php'; ?>
