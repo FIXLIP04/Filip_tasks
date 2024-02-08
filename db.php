18c3b7ac2b9c10abcf6d94c0ac748c778680a723
FIXLIP04 <fixlip04@gmail.com>
2024-02-08 10:52:21 +0200

    shell.hamk.fi in comment

--- db.php
+++ db.php
@@ -4,6 +4,15 @@ $username = "crud_app";     // Replace with your MySQL username
 $password = "pass1234";     // Replace with your MySQL password
 $dbname = "crud_app";       // Replace with the name of your MySQL database
 
+/*
+shell.hamk.fi access
+
+$servername = "localhost"; // Replace with your MySQL server hostname
+$username = "filip23000";     // Replace with your MySQL username
+$password = "uE6oVlHe";     // Replace with your MySQL password
+$dbname = "wp_filip23000";       // Replace with the name of your MySQL database
+*/
+
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
